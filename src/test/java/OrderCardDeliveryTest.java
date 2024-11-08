214748cf4b98e30480d24580b6f4a3be19a04d98
Sergey Bykhavets <sergozby@gamil.com>
2024-11-09 00:13:34 +0300

    Fix naming, add test for Task 1 and 2

--- src/test/java/OrderCardDeliveryTest.java
+++ src/test/java/OrderCardDeliveryTest.java
@@ -1,3 +1,175 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class OrderCardDeliveryTest {
 
+    private String generateDate(int days, String pattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @BeforeEach
+    void setUp() {
+        Selenide.open("http://localhost:9999");
+    }
+
+    @AfterEach
+    void tearDown() {
+        Selenide.closeWindow();
+    }
+
+    // TASK #1
+    // Positive Test
+    @Test
+    void shouldOrderCardDelivery() {
+        String selectDate = generateDate(4, "dd.MM.yyyy");
+
+        $("[data-test-id='city'] .input__control").setValue("Мо");
+        $$("span").filter(visible).find(Condition.exactText("Москва")).click();
+        $("[data-test-id='date'] .input__control").sendKeys(
+                Keys.chord(Keys.SHIFT,Keys.HOME),Keys.BACK_SPACE
+        );
+        $$("[data-test-id='date'] .input__control").filter(visible).first().setValue(selectDate);
+        $$("[data-test-id='name'] .input__control").first().setValue("Иван Салтыков-Щедрин");
+        $$("[data-test-id='phone'] .input__control").first().setValue("+71231234567");
+        $$("[data-test-id='agreement'] span").first().click();
+        $$("button").filter(visible).find(Condition.exactText("Забронировать")).click();
+
+        String notificationContent = $$("[data-test-id='notification'] .notification__content").first().shouldBe(visible, Duration.ofSeconds(15)).text();
+        assertEquals("Встреча успешно забронирована на " + selectDate, notificationContent);
+    }
+
+    // Negative Test - wrong city
+    @Test
+    void shouldNotAllowToSelectNotRussianCity() {
+        // City is not from Russia
+        $("[data-test-id='city'] .input__control").setValue("Минск");
+        $$("button").filter(visible).find(Condition.exactText("Забронировать")).click();
+
+        String warning = $("[data-test-id='city'] .input__sub").text();
+        assertEquals("Доставка в выбранный город недоступна", warning);
+    }
+
+    // Negative Test - wrong date
+    @Test
+    void shouldNotAllowToSelectDateToBeforeLessThen_3_Days() {
+        String selectDate = generateDate(1, "dd.MM.yyyy");
+
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        $("[data-test-id='date'] .input__control").sendKeys(
+                Keys.chord(Keys.SHIFT,Keys.HOME),Keys.BACK_SPACE
+        );
+        // Time is more than 3 days before current date
+        $("[data-test-id='date'] .input__control").setValue(selectDate);
+        $$("button").filter(visible).find(Condition.exactText("Забронировать")).click();
+
+        String warning = $("[data-test-id='date'] .input__sub").text();
+        assertEquals("Заказ на выбранную дату невозможен", warning);
+    }
+
+    // Negative Test - wrong name
+    @Test
+    void shouldNotAllowLatinName() {
+        String selectDate = generateDate(4, "dd.MM.yyyy");
+
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        $("[data-test-id='date'] .input__control").setValue(selectDate);
+        // Latin letters on name
+        $("[data-test-id='name'] .input__control").setValue("John Travolta");
+        $$("button").filter(visible).find(Condition.exactText("Забронировать")).click();
+
+        String warning = $("[data-test-id='name'] .input__sub").text();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", warning);
+    }
+
+    // Negative Test - wrong phone number
+    @Test
+    void shouldNotAllowShortNumber() {
+        String selectDate = generateDate(4, "dd.MM.yyyy");
+
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        $("[data-test-id='date'] .input__control").setValue(selectDate);
+        $$("[name='name']").first().setValue("Иван Салтыков-Щедрин");
+        // Short number
+        $$("[data-test-id='phone'] .input__control").first().setValue("+7123");
+        $$("button").filter(visible).find(Condition.exactText("Забронировать")).click();
+
+        String warning = $("[data-test-id='phone'] .input__sub").text();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", warning);
+    }
+
+    // Negative Test - wrong phone number
+    @Test
+    void shouldNotAllowInvalidNumber() {
+        String selectDate = generateDate(4, "dd.MM.yyyy");
+
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        $("[data-test-id='date'] .input__control").setValue(selectDate);
+        $$("[name='name']").first().setValue("Иван Салтыков-Щедрин");
+        // Plus on end of phone number
+        $$("[data-test-id='phone'] .input__control").first().setValue("71231234567+");
+        $$("button").filter(visible).find(Condition.exactText("Забронировать")).click();
+
+        String warning = $("[data-test-id='phone'] .input__sub").text();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", warning);
+    }
+
+    // Negative Test - without agreement
+    @Test
+    void shouldNotAllowWithoutAgreement() {
+        String selectDate = generateDate(4, "dd.MM.yyyy");
+
+        $("[data-test-id='city'] .input__control").setValue("Москва");
+        $("[data-test-id='date'] .input__control").setValue(selectDate);
+        $$("[name='name']").first().setValue("Иван Салтыков-Щедрин");
+        $$("[data-test-id='phone'] .input__control").first().setValue("+71231234567");
+        $$("button").filter(visible).find(Condition.exactText("Забронировать")).click();
+
+        String warning = $("[data-test-id='agreement'] [role='presentation']").text();
+        assertEquals("Я соглашаюсь с условиями обработки и использования моих персональных данных", warning);
+    }
+
+    // TASK #2
+    // Positive Test - Select city from list and select next month by click
+    @Test
+    void shouldOrderCardDelivery_Advanced() {
+        String selectDate = generateDate(25, "dd.MM.yyyy");
+        String oneDay = generateDate(25, "d");
+        String currentMonth = generateDate(0, "MM");
+        String selectedMoth = generateDate(25, "MM");
+
+        // Enter two first letters
+        $("[data-test-id='city'] .input__control").setValue("Мо");
+        $$("span").filter(visible).find(Condition.exactText("Москва")).click();
+
+        // Check will we need to select next month or not
+        if (currentMonth.equals(selectedMoth)) {
+            $("[data-test-id='date'] .input__icon").click();
+            $$(".popup__container .calendar__day").filter(visible).find(Condition.exactText(oneDay)).click();
+        } else {
+            $("[data-test-id='date'] .input__icon").click();
+            $("[data-step='1'].calendar__arrow_direction_right").click();
+            $$(".popup__container .calendar__day").filter(visible).find(Condition.exactText(oneDay)).click();
+        }
+
+        $$("[data-test-id='name'] .input__control").first().setValue("Иван Салтыков-Щедрин");
+        $$("[data-test-id='phone'] .input__control").first().setValue("+71231234567");
+        $$("[data-test-id='agreement'] span").first().click();
+        $$("button").filter(visible).find(Condition.exactText("Забронировать")).click();
+
+        String notificationContent = $$("[data-test-id='notification'] .notification__content").first().shouldBe(visible, Duration.ofSeconds(15)).text();
+        assertEquals("Встреча успешно забронирована на " + selectDate, notificationContent);
+    }
 }
